8188627a40453609af4a4c4b044b1929719741b8
Brian Thomas Storti <btstorti@gmail.com>
2014-03-14 19:04:40 -0300

    add log messages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_zsh() {
+  echo "installing zsh"
   if [ `uname` = "Linux" ]; then
     sudo apt-get update;
     sudo apt-get install zsh;
@@ -13,11 +14,13 @@ install_oh_my_zsh() {
   install_zsh
 
   if ! [ -d ~/.oh-my-zsh ]; then
+    echo "installing oh-my-zsh"
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
   fi
 }
 
 source_dotfiles() {
+  echo "sourcing dotfiles"
   for file in \
     zshrc gitconfig githelpers \
     gitignore ackrc hushlogin \
@@ -30,6 +33,7 @@ source_dotfiles() {
       mv $original_file $original_file.bkp.$$
     fi
 
+    echo "updating $file"
     ln -sf `pwd`/$file $original_file
   done
 }
@@ -37,5 +41,8 @@ source_dotfiles() {
 install_oh_my_zsh
 source_dotfiles
 
+echo "defininf zsh as the default shell"
 chsh -s `which zsh`
+
+echo "done!"
 exec zsh --login
